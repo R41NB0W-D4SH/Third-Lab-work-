2f7521dc7659cad88e0cbc0c945a66f183d76ddd
R41NB0W-D4SH <54800479+R41NB0W-D4SH@users.noreply.github.com>
2019-09-16 22:31:22 +0300

    Update task2.cpp

--- task2.cpp
+++ task2.cpp
@@ -1,1 +1,22 @@
+#include <iostream>
 
+using namespace std;
+
+int main()
+{
+	setlocale (LC_ALL, "Russian");
+	float a,b,c,d,e;
+	cout <<"Введите перменную A, затем B, потом C"<< endl;
+	cin >> a >> b >> c;
+
+	d = b;
+	e = c;
+	b = a;
+	c = d;
+	a = e;
+
+	cout <<"переменная A = "<< a << endl << "переменная B = "<< b << endl << "переменная C = "<< c << endl;
+	
+	system("pause");
+	return 0;
+}
