615cc30fb74ee7d82dd7d0e79e34ed0877862386
R41NB0W-D4SH <54800479+R41NB0W-D4SH@users.noreply.github.com>
2019-09-16 23:41:26 +0300

    Update task6.cpp

--- task6.cpp
+++ task6.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+#include <cmath>
 
+using namespace std;
+
+int main()
+{
+	setlocale (LC_ALL, "Russian");
+	float a;
+	cout <<"Введите значение переменной A"<< endl;
+	cin >> a;
+
+	a = pow(a,8);
+
+	cout <<"A = "<< a << endl;
+	
+	system("pause");
+	return 0;
+}
