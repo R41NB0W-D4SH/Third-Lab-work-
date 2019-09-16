feb153313f12dbe89ffea0bc2f9865c9b7f097eb
R41NB0W-D4SH <54800479+R41NB0W-D4SH@users.noreply.github.com>
2019-09-16 23:02:51 +0300

    Create task5.cpp

---
+++ task5.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale (LC_ALL, "Russian");
+	float x,y;
+	cout <<"Введите значение переменной X для уравнения y = 4*(x - 3)^6 - 7*(x - 3)^3 + 2"<< endl;
+	cin >> x;
+
+	y = 4 * (pow(x - 3,6)) - 7 * (pow(x - 3,2)) + 2;
+
+	cout <<"Значение функции уравнения y = 4*(x - 3)^6 - 7*(x - 3)^3 + 2 = "<< y << ", при x = "<< x << endl;
+	
+	system("pause");
+	return 0;
+}
